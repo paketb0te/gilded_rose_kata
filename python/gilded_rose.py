0f49011e9b659384d255bce2e5e82680516fc90b
Manuel Stausberg <m.stausberg@pm.me>
2023-09-11 20:33:15 +0200

    remove GildedRose class

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -15,36 +15,32 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
-    def __init__(self, items: list[Item]):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
-                if item.quality > 0:
-                    if item.name != SULFURAS:
-                        item.quality = item.quality - 1
+def update_quality(items: list[Item]):
+    for item in items:
+        if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
+            if item.quality > 0:
+                if item.name != SULFURAS:
+                    item.quality = item.quality - 1
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if item.name == BACKSTAGE_PASSES:
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+        if item.name != SULFURAS:
+            item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.name != AGED_BRIE:
+                if item.name != BACKSTAGE_PASSES:
+                    if item.quality > 0:
+                        if item.name != SULFURAS:
+                            item.quality = item.quality - 1
+                else:
+                    item.quality = item.quality - item.quality
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == BACKSTAGE_PASSES:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != SULFURAS:
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != AGED_BRIE:
-                    if item.name != BACKSTAGE_PASSES:
-                        if item.quality > 0:
-                            if item.name != SULFURAS:
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
