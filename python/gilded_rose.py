6e0979014ae96aaeb1b1e355521c7ea855e41086
Manuel Stausberg <m.stausberg@pm.me>
2023-09-11 21:55:02 +0200

    move updating functions further up

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -15,6 +15,35 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
+def update_default_item(item: Item):
+    decrement = 2 if item.sell_in < 0 else 1
+    new_quality = item.quality - decrement
+    item.quality = max(new_quality, 0)
+
+
+def update_aged_brie(item: Item):
+    increase = 2 if item.sell_in < 0 else 1
+    new_quality = item.quality + increase
+    item.quality = min(new_quality, 50)
+
+
+def update_backstage_passes(item: Item):
+    if 5 <= item.sell_in < 10:
+        increase = 2
+    elif 0 <= item.sell_in < 5:
+        increase = 3
+    elif item.sell_in < 0:
+        increase = -item.quality
+    else:
+        increase = 1
+    new_quality = item.quality + increase
+    item.quality = min(new_quality, 50)
+
+
+def update_sulfuras(item: Item):
+    pass
+
+
 def update_quality_of_all_items(items: list[Item]):
     for item in items:
         update_quality_of_single_item(item)
@@ -31,35 +60,6 @@ def update_quality_of_single_item(item: Item):
         update_sulfuras(item)
     else:
         update_default_item(item)
-
-
-def update_sulfuras(item: Item):
-    pass
-
-
-def update_default_item(item: Item):
-    decrement = 2 if item.sell_in < 0 else 1
-    new_quality = item.quality - decrement
-    item.quality = max(new_quality, 0)
-
-
-def update_backstage_passes(item: Item):
-    if 5 <= item.sell_in < 10:
-        increase = 2
-    elif 0 <= item.sell_in < 5:
-        increase = 3
-    elif item.sell_in < 0:
-        increase = -item.quality
-    else:
-        increase = 1
-    new_quality = item.quality + increase
-    item.quality = min(new_quality, 50)
-
-
-def update_aged_brie(item: Item):
-    increase = 2 if item.sell_in < 0 else 1
-    new_quality = item.quality + increase
-    item.quality = min(new_quality, 50)
 
 
 def update_sell_in_date(item: Item):
