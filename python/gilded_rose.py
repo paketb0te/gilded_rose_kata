d8eee9d47eda7ad587de293cf62ccb2d1a20bd76
Manuel Stausberg <m.stausberg@pm.me>
2023-09-11 21:01:37 +0200

    remove obsolete conditionals

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -24,23 +24,15 @@ def update_quality_of_single_item(item: Item):
     if item.name == AGED_BRIE:
         if item.quality < 50:
             item.quality = item.quality + 1
-            if item.name == BACKSTAGE_PASSES:
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
     elif item.name == BACKSTAGE_PASSES:
         if item.quality < 50:
             item.quality = item.quality + 1
-            if item.name == BACKSTAGE_PASSES:
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if item.sell_in < 11:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+            if item.sell_in < 6:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
     else:
         if item.quality > 0:
             if item.name == SULFURAS:
