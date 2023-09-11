bfca2c620a345c5f5a3b6dc40f09a90238a67dea
Manuel Stausberg <m.stausberg@pm.me>
2023-09-11 20:54:07 +0200

    invert conditionals for sulfuras

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -23,7 +23,9 @@ def update_quality_of_all_items(items: list[Item]):
 def update_quality_of_single_item(item: Item):
     if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
         if item.quality > 0:
-            if item.name != SULFURAS:
+            if item.name == SULFURAS:
+                pass
+            else:
                 item.quality = item.quality - 1
     else:
         if item.quality < 50:
@@ -35,13 +37,17 @@ def update_quality_of_single_item(item: Item):
                 if item.sell_in < 6:
                     if item.quality < 50:
                         item.quality = item.quality + 1
-    if item.name != SULFURAS:
+    if item.name == SULFURAS:
+        pass
+    else:
         item.sell_in = item.sell_in - 1
     if item.sell_in < 0:
         if item.name != AGED_BRIE:
             if item.name != BACKSTAGE_PASSES:
                 if item.quality > 0:
-                    if item.name != SULFURAS:
+                    if item.name == SULFURAS:
+                        pass
+                    else:
                         item.quality = item.quality - 1
             else:
                 item.quality = item.quality - item.quality
