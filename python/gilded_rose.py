408fc3205032208ef0cc806232ae476dd5407323
Manuel Stausberg <m.stausberg@pm.me>
2023-09-11 19:53:35 +0200

    add type hints for Item class

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
 
-    def __init__(self, items):
+class Item:
+    def __init__(self, name: str, sell_in: int, quality: int):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class GildedRose(object):
+    def __init__(self, items: list[Item]):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if (
+                item.name != "Aged Brie"
+                and item.name != "Backstage passes to a TAFKAL80ETC concert"
+            ):
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
@@ -34,13 +47,3 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
-
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,7 +5,7 @@ from gilded_rose import GildedRose, Item
 
 
 def test_foo():
-    items = [Item("foo", 0, 0)]
+    items = [Item(name="foo", sell_in=0, quality=0)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
     assert items[0].name == "foo"
