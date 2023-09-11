cdc9afc619c39bbfe663e7a21428179f77204f65
Manuel Stausberg <m.stausberg@pm.me>
2023-09-11 21:33:09 +0200

    extract update_default_item

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -30,11 +30,15 @@ def update_quality_of_single_item(item: Item):
     elif item.name == SULFURAS:
         pass
     else:
+        update_default_item(item)
+
+
+def update_default_item(item: Item):
+    if item.quality > 0:
+        item.quality = item.quality - 1
+    if item.sell_in < 0:
         if item.quality > 0:
             item.quality = item.quality - 1
-        if item.sell_in < 0:
-            if item.quality > 0:
-                item.quality = item.quality - 1
 
 
 def update_backstage_passes(item: Item):
